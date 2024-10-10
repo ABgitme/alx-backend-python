4bde2c619a7a65972bbc93706eea9c568e2050a5
ABgitme <abebekezhabesha@gmail.com>
2024-10-10 06:33:59 +0100

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+This module demonstrates variable type annotations for
+different data types including integers, floats, booleans, and strings.
+"""
+
+# Variable definitions with type annotations
+a: int = 1
+"""
+a (int): An integer with the value 1.
+"""
+
+pi: float = 3.14
+"""
+pi (float): A floating-point number
+representing the value of pi as 3.14.
+"""
+
+i_understand_annotations: bool = True
+"""
+i_understand_annotations (bool): A boolean with the value True,
+indicating understanding of annotations.
+"""
+
+school: str = "Holberton"
+"""
+school (str): A string containing the name "Holberton".
+"""
