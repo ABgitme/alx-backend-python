fd260cbf6d4e2316f8628deb43248e0d100b0a35
ABgitme <abebekezhabesha@gmail.com>
2024-10-10 06:15:04 +0100

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to return the floor value of a floating-point number.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the given floating-point number.
+
+    Args:
+        n (float): The floating-point number to floor.
+
+    Returns:
+        int: The largest integer less than or equal to the given float.
+    """
+    return math.floor(n)
