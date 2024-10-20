08229bec504e60681548a9ebf799fabc21cc6d44
ABgitme <abebekezhabesha@gmail.com>
2024-10-20 10:27:31 +0100

    0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,23 @@
+import asyncio
+import random
+"""
+This module defines an asynchronous generator
+function that yields random numbers
+between 0 and 10 with a delay of 1 second between each yield.
+"""
+
+
+async def async_generator():
+    """
+    Asynchronously generates 10 random numbers between 0 and 10.
+
+    This coroutine loops 10 times,
+    waiting for 1 second asynchronously on each iteration,
+    then yields a random floating-point number in the range [0, 10).
+
+    Yields:
+        float: A random number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)  # asynchronously wait for 1 second
+        yield random.uniform(0, 10)  # yield a random float between 0 and 10
